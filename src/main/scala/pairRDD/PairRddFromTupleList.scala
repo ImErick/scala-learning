b279e4b3e025e8c9d9c809f2e9a95cf787baaa7e
ImErick <3r1ck.442@gmail.com>
2019-06-16 16:47:15 -0500

    using coalesce

--- src/main/scala/pairRDD/PairRddFromTupleList.scala
+++ src/main/scala/pairRDD/PairRddFromTupleList.scala
@@ -6,13 +6,15 @@ import org.apache.spark.{SparkConf, SparkContext}
 object PairRddFromTupleList {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
-    val conf = new SparkConf().setAppName("create").setMaster("local[1]")
+    val conf = new SparkConf().setAppName("create").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val tuple = List(("Lily", 23), ("Jack", 29), ("Mary", 29), ("James", 8))
-    val pairRDD = sc.parallelize(tuple) // parallelize como la normalmente pasamos la info harcodeada a un RDD
+    val pairRDD = sc.parallelize(tuple) // pasamos la info harcodeada a un RDD
 
-    // para que sirve coalesce? persistir en disco (?)
-    pairRDD.coalesce(1).saveAsTextFile("out/pair_rdd_from_tuple_list")
+    //println("tamaño de la particion " + pairRDD.partitions.size) // la particion por default usando todos mis CPU es de 8
+
+    // para que sirve coalesce? coalesce method reduces the number of partitions in a DataFrame
+    pairRDD.coalesce(1).saveAsTextFile("out/pair_rdd_from_tuple_list") // el 3 es de test
   }
 }
