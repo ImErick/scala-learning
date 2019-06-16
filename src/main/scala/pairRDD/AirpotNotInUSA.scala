23a99cc0c13ac2d326e38235fe1941357db9960e
ImErick <3r1ck.442@gmail.com>
2019-06-16 17:13:58 -0500

    small fix comment to my understate

--- src/main/scala/pairRDD/AirpotNotInUSA.scala
+++ src/main/scala/pairRDD/AirpotNotInUSA.scala
@@ -7,15 +7,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 object AirpotNotInUSA {
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.ERROR)
-    val conf = new SparkConf().setAppName("airports").setMaster("local")
+    val conf = new SparkConf().setAppName("airports").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val airportsRDD = sc.textFile("files/airports.text")
 
     val airportPairRDD = airportsRDD.map(line => (line.split(Utils.COMMA_DELIMITER)(1),
       line.split(Utils.COMMA_DELIMITER)(3)))
-    val airportsNotInUSA = airportPairRDD.filter(keyValue => keyValue._2 != "\"United States\"")
 
+    // print a RDD: println(airportPairRDD.collect().foreach(println))
+    val airportsNotInUSA = airportPairRDD.filter(keyValue => keyValue._2 != "\"United States\"")
     airportsNotInUSA.saveAsTextFile("out/airports_not_in_usa_pair_rdd.text")
   }
 }
