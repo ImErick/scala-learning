f7c7db0a1d4754ee10e93c3fefd163fe03441ea6
ImErick <3r1ck.442@gmail.com>
2019-06-16 18:30:11 -0500

    fix a typo and understand sortByKey

--- src/main/scala/pairRDD/SortedWordCount.scala
+++ src/main/scala/pairRDD/SortedWordCount.scala
@@ -15,8 +15,8 @@ object SortedWordCount {
     val wordPairRdd = wordRdd.map(word => (word, 1))
     val wordToCountPairs = wordPairRdd.reduceByKey((x, y) => x + y)
 
-    val countToWordParis = wordToCountPairs.map(wordToCount => (wordToCount._2, wordToCount._1))
-    val sortedCountToWordParis = countToWordParis.sortByKey(ascending = false)
+    val countToWordPairs = wordToCountPairs.map(wordToCount => (wordToCount._2, wordToCount._1))
+    val sortedCountToWordParis = countToWordPairs.sortByKey(ascending = false) // ordenando de mayor coincidencias a menor
     val sortedWordToCountPairs = sortedCountToWordParis.map(countToWord => (countToWord._2, countToWord._1))
 
     for ((word, count) <- sortedWordToCountPairs.collect())
