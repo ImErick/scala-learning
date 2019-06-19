ecf704ab72ff4930529210d19161b0e66fc524be
ImErick <3r1ck.442@gmail.com>
2019-06-19 11:51:10 -0500

    finish class 3

--- src/main/scala/basics/LearningScala3.scala
+++ src/main/scala/basics/LearningScala3.scala
@@ -11,11 +11,21 @@ object LearningScala3 {
     x * x * x
   }
 
-  //TODO: en Scala se pueden pasar funciones como parametros de otras funciones, OLOVORGO
+  //en Scala se pueden pasar funciones como parametros de otras funciones, OLOVORGO
+  def functionInsideFunction(x: Int, f: Int => Int): Int = {f(x)}
 
   def main(args: Array[String]): Unit = {
     println("cuadrado de 2: " + square(2))
     println("cubo de 2: " + cube(2))
+
+    // funcion dentro de la funcion
+    println("funcion dentro de la funcion " + functionInsideFunction(6, cube))
+
+    // tambien existen las funciones lambda o anonimas
+    println("expresion lambda " + functionInsideFunction(5, x => x * 10))
+
+    // un poco mas complejo...
+    println("mas complejo: " + functionInsideFunction(10, x => {val y = x + 1; y * y}))
   }
 
 }
