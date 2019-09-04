0d86423beed714fe1f37c1e9a701c5c568a54924
ImErick <3r1ck.442@gmail.com>
2019-09-03 21:33:01 -0500

    add case classes example

--- src/main/scala-spark/scala/CaseClasses.scala
+++ src/main/scala-spark/scala/CaseClasses.scala
@@ -1,5 +1,23 @@
 package scala
 
-object CaseClasses {
+object CaseClasses extends App {
+
+  case class Person(name: String, age: Int)
+
+  // los parametros del constructor YA son campos (ya no se necesita 'val'
+  val erick = Person("Erick", 28)
+  println(erick.name)
+
+  // es accesible a toString
+  println(erick.toString)
+  println(erick) // lo mismo de arriba
+
+  // equals y hashCode esta implementado
+  val otroErick = Person("Erick", 28)
+  println(erick == otroErick)
+
+  // tienen metodo pa copiarse
+  val nuevoErick = erick.copy() // se le puede pasar valores nuevos en los parentesis
+
 
 }
